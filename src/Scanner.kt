c049988a682c72576a34ed35d420aee2c4e74b6e
stegmanh <stegman.holden@gmail.com>
2019-01-08 23:25:26 -0500

    add support for scanner progression

--- src/Scanner.kt
+++ src/Scanner.kt
@@ -1,9 +1,28 @@
 package lox
 
+import lox.TokenType.*
+
 class Scanner(val source: String) {
-    val tokens: List<Token> = ArrayList()
+    val tokens: ArrayList<Token> = ArrayList()
+    private var start = 0
+    private var current = 0
+    private var line = 0
 
     fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
+            start = current
+            scanToken()
+        }
+
+        tokens.add(Token(EOF, "", null, line))
         return tokens
+    }
+
+    fun isAtEnd(): Boolean {
+        return current >= source.length
+    }
+
+    fun scanToken() {
+        
     }
 }
