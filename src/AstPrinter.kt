04639ce8c277b7ee230c1c4d7f731f60eb4f312c
stegmanh <stegman.holden@gmail.com>
2019-01-21 22:28:20 -0500

    remove test method

--- src/AstPrinter.kt
+++ src/AstPrinter.kt
@@ -37,17 +37,3 @@ class AstPrinter: Expr.Visitor<String> {
         return builder.toString()
     }
 }
-
-fun main(args: Array<String>) {
-    val expression = Expr.Binary(
-            Expr.Unary(
-                    Token(TokenType.MINUS, "-", null, 1),
-                    Expr.Literal(123)
-            ),
-            Token(TokenType.STAR, "*", null, 1),
-            Expr.Grouping(
-                    Expr.Literal(45.67)
-            )
-    )
-    println(AstPrinter().print(expression))
-}
